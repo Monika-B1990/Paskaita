5efc5fc53f701944a90c8d7c9bb257ec292d1676
Monika <m.balciunaite@gmail.com>
2020-07-11 11:49:09 +0300

    New task

--- src/com/company/New.java
+++ src/com/company/New.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class New {
+    public static void main(String[] args) {
+        int[] array = new int[300];
+        int arraynew = 0;
+        while (arraynew > 300) ;
+        arraynew++;
+        System.out.println(array);
+
+    }
 }
+
